5479927ed72ee353b38c5e760e2c7e742fbc4ec7
yumori59 <yuyamorishita26@gmail.com>
2024-03-11 22:12:29 +0900

    setup

---
+++ next_project/src/app/page.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = (): Element => {
+  return (
+    <>
+      <main>
+        <div className="p-10 font-bold text-red-500">Hello TailwindCss</div>
+      </main>
+    </>
+  )
+}
+
+export default Home
